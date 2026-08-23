01a8976b0d6064f116713db0170095ec716fa2ae
Priya Saikia <saikiapriya049@gmail.com>
2026-08-23 12:44:54 +0530

    Add DSA practice problems

---
+++ sformula.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+import java.lang.Math;
+
+class Solution{
+    public static void main(String []args){
+        Scanner in = new Scanner(System.in);
+        System.out.println("q: ");
+        int q=in.nextInt();
+        for(int i=0;i<q;i++){
+            System.out.println("a: ");
+            int a = in.nextInt();
+            System.out.println("b: ");
+            int b = in.nextInt();
+            System.out.println("n: ");
+            int n = in.nextInt();
+            int sum=a;
+            for(int j=0; j<n; j++){
+                sum=sum+(int)Math.pow(2,j)*b;
+                System.out.print(sum+" ");
+            }
+            System.out.println();
+        }
+        in.close();
+    }
+}
---
+++ skip.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class skip{
+    public static void main (String[] args){
+        Scanner sc=new Scanner(System.in);
+        String name= sc.nextLine();
+        
+        int age= sc.nextInt();
+
+       
+        System.out.println("n "+ name);
+        System.out.println("a "+ age);
+
+
+    }
+}
